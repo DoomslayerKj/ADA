b76858f153e17886edcaac47fbb40e4b89ab95d2
VaradVithalKj <47701278+DoomslayerKj@users.noreply.github.com>
2020-03-11 12:56:31 +0530

    Create joohn_venk.c

---
+++ joohn_venk.c
@@ -0,0 +1,15 @@
+#include<stdio.>
+
+void swap(int *x, int *y){
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+int k_element(int p[], int dir[], int n){
+    int i,pre,min
+    min = p[0];
+    for(i = 0;i < n; i++){
+        if(p[i] > min && dir[i] == dir[i+1])
+    }
+}
